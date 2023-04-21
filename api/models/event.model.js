6f15752cc9c0d4583f45cb480faf27574467e1b6
Garin90 <alex.garrido.baldrich@gmail.com>
2023-04-21 21:58:20 +0200

    forum commentand topic model

--- api/models/event.model.js
+++ api/models/event.model.js
@@ -9,8 +9,8 @@ const eventSchema = new Schema({
   description: {
     type: String,
     required: true,
-    minlength: [20, 'Claim description needs at least 20 chars'],
-    maxlength: [140, 'Claim description max 140 chars']
+    minlength: [20, 'Event description needs at least 20 chars'],
+    maxlength: [140, 'Event description max 140 chars']
   },
   date: { 
     type: Date,
